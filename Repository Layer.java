560adcf3deab8bcab47fc6865a91b5228482ac0a
Bokang Isaac Sowe Moalolo <112638618+Sowe-coder@users.noreply.github.com>
2026-02-23 01:17:07 +0200

    Add InsurancePolicyRepository interface

--- Repository Layer.java
+++ Repository Layer.java
@@ -13,3 +13,19 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
+
+
+package com.insurance.repository;
+
+import com.insurance.model.InsurancePolicy;
+import com.insurance.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InsurancePolicyRepository extends JpaRepository<InsurancePolicy, Long> {
+    List<InsurancePolicy> findByUser(User user);
+    List<InsurancePolicy> findByStatus(String status);
+}
